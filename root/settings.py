ae594aefa1b3c0aaeb52d2a33a2a2de73bd81fdd
kam1lovic <muhammadleo1010@gmail.com>
2024-07-24 20:54:47 +0500

    commit

--- root/settings.py
+++ root/settings.py
@@ -1,3 +1,4 @@
+import os.path
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -146,6 +147,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR / 'static')
 
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = '/'
